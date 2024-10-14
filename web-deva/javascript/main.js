065bd373804b23d93d0201c6366fd0bb29e2d157
MyLastNameCk <111222909+MyLastNameCk@users.noreply.github.com>
2024-10-14 08:36:06 +0700

    Update main.js all whatsapp section

--- web-deva/javascript/main.js
+++ web-deva/javascript/main.js
@@ -13,25 +13,18 @@ function revealMessage() {
 
 // WhatsApp Message
 function sendWhatsAppMessage() {
-    // Ambil pesan yang ditulis oleh pengguna
     var message = document.getElementById('whatsappMessage').value;
 
-    // Cek jika pesan kosong, tampilkan alert
     if (message.trim() === '') {
         alert('Tolong tulis pesan terlebih dahulu!');
         return;
     }
-
+    
+    var formattedMessage = "from Vivi to you: " + message;
     var encodedMessage = encodeURIComponent(message);
 
-    // Nomor telepon tujuan (format internasional tanpa + atau spasi)
     var phoneNumber = '6285904471135'; // Ganti dengan nomor tujuan
     var whatsappUrl = "https://wa.me/" + 6285904471135 + "?text=" + encodedMessage;
 
-    // Buat URL WhatsApp
-    // var url = `https://wa.me/${6285904471135}?text=${encodeURIComponent(message)}`;
-
-    // Arahkan pengguna ke URL tersebut
-    // window.location.href = 'https://wa.me/qr/EA776HKALFVDJ1';
     window.open(whatsappUrl, "_blank");
 }
